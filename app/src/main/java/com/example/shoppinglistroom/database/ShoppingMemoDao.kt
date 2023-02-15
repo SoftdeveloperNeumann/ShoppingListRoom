3123428ade3e3730db564b7ac15ddba6886338a4
Frank <f.neumann.berlin@googlemail.com>
2023-02-15 09:24:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglistroom/database/ShoppingMemoDao.kt
@@ -0,0 +1,22 @@
+package com.example.shoppinglistroom.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface ShoppingMemoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdate(memo: ShoppingMemo)
+
+    @Delete
+    suspend fun delete(memo: ShoppingMemo)
+
+    @Query(value = "SELECT * FROM shopping_list")
+    fun getAllShoppingMemos(): LiveData<List<ShoppingMemo>>
+
+    // dient nur als Beispiel für weitere Möglichkeiten
+    @Query(value = "SELECT * FROM shopping_list WHERE quantity = :quantity")
+    fun getMemosByQuantity(quantity: Int): LiveData<List<ShoppingMemo>>
+}
