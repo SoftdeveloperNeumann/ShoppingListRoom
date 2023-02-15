1ef4d8fe59c2084d908a190f9ca131d5a163b378
Frank <f.neumann.berlin@googlemail.com>
2023-02-15 11:10:18 +0100

    Adapter V1 ist Fertiggestellt

--- app/src/main/java/com/example/shoppinglistroom/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistroom/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.shoppinglistroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglistroom.adapter.ShoppingMemoListAdapter
 import com.example.shoppinglistroom.databinding.ActivityMainBinding
 import com.example.shoppinglistroom.viewmodel.ShoppingMemoViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val adapter: ShoppingMemoListAdapter = ShoppingMemoListAdapter()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var shoppingMemoViewModel: ShoppingMemoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvShoppingMemos.layoutManager = LinearLayoutManager(this)
+        binding.rvShoppingMemos.adapter = adapter
 
 
     }
